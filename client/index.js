4ff3f35a00ca4007d84ea7a39fb5fb4cf4694a1e
arivusurya <arivusurya.3@gmail.com>
2023-12-29 17:03:17 +0530

    ares

--- client/index.js
+++ client/index.js
@@ -18,3 +18,35 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+// billing page
+
+amount = unit * price;
+currentRow.querySelector("#amount").value = amount;
+overallSum();
+
+//Get the overall sum/Total
+overallSum = () => {
+  var arr = document.getElementsByName("amount");
+  var total = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].value) {
+      total += +arr[i].value;
+    }
+    document.getElementById("total").value = total;
+  }
+};
+
+//Delete row from the table
+tBody.addEventListener("click", (e) => {
+  let el = e.target;
+  const deleteROW = e.target.getAttribute("action");
+  if (deleteROW == "delete") {
+    delRow(el);
+    overallSum();
+  }
+});
+
+//Target row and remove from DOM;
+delRow = (el) => {
+  el.parentNode.parentNode.parentNode.removeChild(el.parentNode.parentNode);
+};
